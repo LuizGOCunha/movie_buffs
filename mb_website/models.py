755bf27b6d51cdbef8e4fa57d865df2d31823279
Luiz <valuiz1@gmail.com>
2023-01-02 20:35:47 -0300

    Application returns movie info, no pagination yet, can return keyerrors with some of the movie info

--- mb_website/models.py
+++ mb_website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=500)
+    overview = models.TextField()
+    release_date = models.DateField()
+    rating = models.FloatField()
+
--- mb_website/views.py
+++ mb_website/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render
-from .forms import SearchBar
 from django.core.handlers.wsgi import WSGIRequest
+from django.core.paginator import Paginator
+
 from .tmdb import search_movies
+from .forms import SearchBar
+from .models import Movie
 
 # Create your views here.
 
 def search_bar(request:WSGIRequest):
     context = {}
     context['form'] = SearchBar
+    object_list = list(range(10))
     print("This is the request:", type(request))
     print("resquest.get:", request.GET)
-    if request.GET:
-        search = context['search']
-#        results = search_movies(search)
+    if 'search' in request.GET.keys():
+        # Get the search from the parameters
+        search = request.GET['search']
+        # Search through the API
+        results = search_movies(search)
+        all_results = []
+        for page in results.values():
+            all_results += page
+        models = []
+        for movie in all_results:
+            model = Movie(title=movie["title"], overview=movie["overview"], release_date=movie["release_date"], rating=movie['vote_average'])
+            models.append(model)
 
+        context['movies'] = models
+        
     return render(request, "search_bar.html", context)
